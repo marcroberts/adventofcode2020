e0661e48bbb79bd1169dd84d0ead9c3c272866ee
Marc Roberts <marc@marcroberts.info>
2020-12-07 11:11:41 +0000

    Initial commit

---
+++ src/bin/day1.rs
@@ -0,0 +1,40 @@
+use std::env;
+use std::io;
+use std::io::BufReader;
+use std::io::BufRead;
+use std::fs::File;
+
+fn main() {
+  let args: Vec<String> = env::args().collect();
+  let filename = &args[1];
+
+	let mut values: Vec<u32> = Vec::new();
+
+	let file = File::open(filename).unwrap();
+	let reader = io::BufReader::new(file);
+
+	// load the values
+	for (index, line) in reader.lines().enumerate() {
+		let number: u32 = line.unwrap().trim().parse().unwrap();
+		values.push(number);
+	}
+
+	// loop on all number, => a
+	'outer: for (a_index, a) in values.iter().enumerate() {
+
+
+		for (b_index, b) in values.iter().enumerate() {
+			if b_index < a_index {
+				continue;
+			}
+
+			if a + b == 2020 {
+				println!("{} + {} = 2020. Sum is {}", a, b, a*b);
+				break 'outer;
+			}
+		}
+
+
+	}
+
+}
